1e21d7da06f23577d0be09bbf0a9522da26a9c39
swathimeenal <swathimeenal1994@gmail.com>
2023-08-29 10:41:35 +0530

    alert

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const button =document.createElement("button");
 button.setAttribute("type","button")
 button.innerText = "Submit"
+button.addEventListener("click",foo);
+foo()
+{
+    alert(" 🌷🌹🌼Happy 🌺 Onam💐🌾🌸");
+}
