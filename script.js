9c1551cfb96438c6d76336859c7c0008deca2c10
swathimeenal <swathimeenal1994@gmail.com>
2023-08-29 12:04:50 +0530

    update

--- script.js
+++ script.js
@@ -2,7 +2,34 @@ const button =document.createElement("button")
 button.setAttribute("type","button")
 button.innerText = "click here to wish"
 button.addEventListener("click",foo);
+document.body.append(button)
 function foo()
 {
     alert(" 🌷🌹🌼Happy 🌺 Onam💐🌾🌸");
 }
+const btn = document.getElementById("btn")
+btn.addEventListener("mouseover",foo1)
+function foo1()
+{
+    document.body.style.backgroundColor="red"
+}
+btn.addEventListener("mouseout",foo2)
+function foo2()
+{
+    document.body.style.backgroundColor=""
+}
+//div
+
+const task= document.getElementById("div1")
+task.addEventListener("mouseover",foo3)
+function foo3()
+{
+    document.task.style.backgroundColor="blue"
+}
+task.addEventListener("mouseout",foo4)
+function foo4()
+{
+    document.task.style.backgroundColor=""
+}
+document.body.append(task)
+
